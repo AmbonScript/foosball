5d66a76728dc4398ccc44f38882019c94190c8d1
Wouter van Ginneken <wfvanginneken@gmail.com>
2025-01-02 11:47:03 +0100

    bla

--- slot.py
+++ slot.py
@@ -30,7 +30,7 @@ class Slot:
             if self.__done_placing(): return True
             else:
                 from slot_factory import SlotFactory
-                slot_factory: SlotFactory = SlotFactory(copy.deepcopy(self))
+                print(SlotFactory(copy.deepcopy(self)).choose_next_slot())
                 self.__next_slot = Slot(self.__number + 1, self.__next_slot)
                 if self.__next_slot.place_players(): return True
                 else: self.__next_slot = first_slot
