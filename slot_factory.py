d48f7a670c7367b21f018277c85c88fb70862a93
Wouter van Ginneken <wfvanginneken@gmail.com>
2025-01-02 11:27:17 +0100

    introduceer SlotFactory

---
+++ slot_factory.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+from player import Player
+from team import Team
+from position import Position
+from result import Result
+from typing import List
+import math
+
+class SlotFactory:
+
+    def __init__(self, slot: Slot):
+        self.__slot = slot
