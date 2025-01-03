e8c622b83ec3ed2325258ebd3ae3587d24bde810
Wouter van Ginneken <wfvanginneken@gmail.com>
2025-01-03 11:28:31 +0100

    methodes SlotFactory statisch gemaakt

--- slot.py
+++ slot.py
@@ -34,7 +34,7 @@ class Slot:
             if self.__done_placing(): return True
             else:
                 from slot_factory import SlotFactory
-                next_slot_number: int = SlotFactory(copy.deepcopy(self)).choose_next_slot_number()
+                next_slot_number: int = SlotFactory.create_slot_number(copy.deepcopy(self))
                 self.__next_slot = Slot(next_slot_number, self.__next_slot)
                 if self.__next_slot.place_players(): return True
                 else: self.__next_slot = first_slot
--- slot_factory.py
+++ slot_factory.py
@@ -9,23 +9,26 @@ import math
 import random
 
 class SlotFactory:
-    
-    def __init__(self, slot: Slot):
-        self.__slot = slot
-    
-    def choose_next_slot_number(self) -> int:
-        base_nr: int = self.__get_base_nr()
-        occupied_slots: List[Slot] = self.__get_occupied_slots()
-        open_slot_nrs: List[int] = self.__get_open_slot_nrs(base_nr, occupied_slots)
+    __slot: Slot = None
+
+    @staticmethod
+    def create_slot_number(slot: Slot) -> int:
+        SlotFactory.__slot = slot
+        base_nr: int = SlotFactory.__get_base_nr()
+        occupied_slots: List[Slot] = SlotFactory.__get_occupied_slots()
+        open_slot_nrs: List[int] = SlotFactory.__get_open_slot_nrs(base_nr, occupied_slots)
         return random.choice(open_slot_nrs)
     
-    def __get_base_nr(self) -> int:
-        return (self.__slot.match - 1) * 4
+    @staticmethod
+    def __get_base_nr() -> int:
+        return (SlotFactory.__slot.match - 1) * 4
     
-    def __get_occupied_slots(self) -> List[Slot]:
-        return self.__slot.determine_occupied_slots_in_match()
+    @staticmethod
+    def __get_occupied_slots() -> List[Slot]:
+        return SlotFactory.__slot.determine_occupied_slots_in_match()
 
-    def __get_open_slot_nrs(self, base_nr: int, occupied_slots: List[Slot]) -> List[int]:
+    @staticmethod
+    def __get_open_slot_nrs(base_nr: int, occupied_slots: List[Slot]) -> List[int]:
         if len(occupied_slots) < 4:
             open_slot_nrs: List[int] = [num + base_nr for num in [1, 2, 3, 4]]
             for slot in occupied_slots:
