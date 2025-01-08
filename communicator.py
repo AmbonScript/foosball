95016501a077e66210415170c763b4246c2b5045
Wouter van Ginneken <wfvanginneken@gmail.com>
2025-01-08 10:57:21 +0100

    dvfvsfbgf

--- communicator.py
+++ communicator.py
@@ -222,6 +222,7 @@ class Communicator:
         Communicator.__shout("PLAY")
         Communicator.__flash_table()
         Communicator.__shout("FOOSBALL!!!!")
+        Communicator.__wiggle_table()
 
     
     def __shout(word: str):
@@ -237,14 +238,23 @@ class Communicator:
         Communicator.__show_table(0)
         time.sleep(1)
 
-    def __show_table(start_line: int):
+    def __show_table(start_line: int, table = None):
+        if table is None:
+            table = Communicator.table
         Communicator.clear_screen()
         print()
         print()
         print()
-        for i in range(start_line, len(Communicator.table)):
-            print(Communicator.table[i])
-
+        for i in range(start_line, len(table)):
+            print(table[i])
+    
+    def __wiggle_table():
+        Communicator.clear_screen()
+        wiggle: bool = True
+        Communicator.__show_table(0)
+        for i in range(10):
+            Communicator.__show_table(0)
+            time.sleep(1)
     
     table: List[str] = [
         "                              [#]                              [#]               [#]      [#]     ",
